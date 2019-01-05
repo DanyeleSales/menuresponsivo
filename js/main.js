4212d32bd16103e4466109a37d11938c20c09a77
Danyele Sales <danyelesales@hotmail.com>
2019-01-05 12:23:59 -0200

    modificando js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
-// selecionar o botão menu
-// selecionar a navegação
-// fazer função d click para abrir efechar menu
-    // verificar se a navegação possuir a classe is-visible no click, retirá-la, caso contrário vai acrescentar
-
-    const button = document.querySelector(".btn-mobile")
+   const button = document.querySelector(".btn-mobile")
     const menuMobile = document.querySelector(".nav-primary");
 
 
@@ -11,11 +6,10 @@ button.addEventListener( 'click', function(e){
     e.preventDefault();
  
     if(menuMobile.classList.contains("nav-primary")){
-        menuMobile.classList.remove("nav-primary");
-        menuMobile.classList.add("nav-primary.is-visible")
+        menuMobile.classList.remove(".nav-primary");
+        menuMobile.classList.add("is-visible")
     }else{
-        menuMobile.classList.remove("nav-primary.is-visible");
-        menuMobile.classList.add("nav-primary")
-    }
+        menuMobile.classList.remove("is-visible");
+            }
 
 })
